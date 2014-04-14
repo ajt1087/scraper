d667131b4a7b89caccac7333130ac3c1f2a6cbea
Andrew <andrew.thoma1@gmail.com>
2014-04-14 02:34:06 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Scraper::Application.config.secret_key_base = '996c36a478d3ab335d0f155c50b492b588bca56ed25252237a4190bf8f85775ed03204dbc9ff687da6ee76791adf57f29bba797584caf21679b9c433cedecb40'
